db78ff4bbec828c9ea373cfa8451b40fdd519c6c
Milind Singh <milind7@live.com>
2022-08-13 14:33:32 +0530

    response body fixed

--- preview/get.ts
+++ preview/get.ts
@@ -1,65 +1,80 @@
-'use strict';
+"use strict";
 
-import { DynamoDB } from 'aws-sdk';
-import { get } from './lib/scraper';
-import { Result } from './models/result';
+import { DynamoDB } from "aws-sdk";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { get } from "./lib/scraper";
+import { Result } from "./models/result";
 
 const options = {
   region: "localhost",
   endpoint: "http://localhost:8000",
   accessKeyId: "access_key",
-  secretAccessKey: "secret_key"
-}
+  secretAccessKey: "secret_key",
+};
 
-let isOffline = () => process.env.IS_OFFLINE
+let isOffline = () => process.env.IS_OFFLINE;
 
-const dynamoDb = isOffline() ? new DynamoDB.DocumentClient(options) : new DynamoDB.DocumentClient()
+const dynamoDb = isOffline()
+  ? new DynamoDB.DocumentClient(options)
+  : new DynamoDB.DocumentClient();
 
-
-module.exports.get = async (event) => {
-  const refresh = event?.query?.refresh
+module.exports.get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const refresh = Boolean(event?.queryStringParameters?.refresh);
+  const url = event.pathParameters.url;
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Key: {
-        url: event.path.url,
+      url: url,
     },
   };
 
   // fetch result from the database
   try {
     const result = await dynamoDb.get(params).promise();
-  
-    if (refresh == undefined || Object.keys(result).length === 0) {
-      const newUrl = Buffer.from(event.path.url, 'base64').toString('ascii');
+
+    if (refresh == true || Object.keys(result).length === 0) {
+      const newUrl = Buffer.from(url, "base64").toString("ascii");
       const data: Result = await get(newUrl);
       result.Item = {};
       result.Item.data = data;
       let record = {
         TableName: process.env.DYNAMODB_TABLE,
         Item: {
-            url: event.path.url,
-            data: data
+          url: url,
+          data: data,
         },
-      }
-      await dynamoDb.put(record).promise()
-      console.log("fetching new")
+      };
+      await dynamoDb.put(record).promise();
+      console.log("fetching new");
     }
-    
+
     let response = {
-      status: 200,
-      data: result.Item.data,
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTP
+      },
+      body: JSON.stringify({
+        status: 200,
+        data: result.Item.data,
+      })
     };
 
-  return response;
-  } catch(error) {
+    return response;
+  } catch (error) {
     // handle potential errors
-    console.log(error)
+    console.log(error);
     if (error) {
       return {
-        status: error.statusCode || 501,
-        error:  error.message,
-        data: {}
-      };
+        statusCode: error.statusCode || 501,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTP
+        },
+        body: "{}"
+      }
     }
   }
 };
