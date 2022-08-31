00eef4cfdc137ff89316a2e65f487ea352419f19
Milind Singh <milind7@live.com>
2022-08-31 13:10:32 +0530

    feat: github serverless aws deploy action added

--- test/unit/scraper.test.ts
+++ test/unit/scraper.test.ts
@@ -3,13 +3,13 @@ import { parse } from '../../src/lib/scraper';
 describe('Scraper Testing', () => {
   describe('Parser Testing', () => {
     it('Manual parser with html empty data', async () => {
-      const url = "https://test.com";
-      const html = "<html></html>";
+      const url = 'https://test.com';
+      const html = '<html></html>';
       const result = parse(url, html);
       expect(result).not.toBeNull();
       expect(result.url).toEqual(url);
-      expect(result.name).toEqual("");
-      expect(result?.meta?.parser).toEqual("manual");
+      expect(result.name).toEqual('');
+      expect(result?.meta?.parser).toEqual('manual');
       expect(result?.meta?.modifiedDate).not.toBeUndefined();
     });
   });
